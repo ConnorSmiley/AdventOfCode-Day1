dbf4861dd5762381639b2433d133a0a4bd00be05
Connor Smiley <moxiesmiley@gmail.com>
2022-12-01 19:34:43 +0900

    day-1

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,22 +1,22 @@
 #Part One
 List = [l.strip() for l in open('./list.py')]
-L = []
+k = []
 for elf in ('\n'.join(List)).split('\n\n'):
     i = 0
     for x in elf.split('\n'):
         i += int(x)
-    L.append(i)
-    print(sorted(L))
+    k.append(i)
+    print(sorted(k))
 
 #Part Two
 List = [l.strip() for l in open('./list.py')]
-L = []
+k = []
 for elf in ('\n'.join(List)).split('\n\n'):
     i = 0
     for x in elf.split('\n'):
         i += int(x)
-    L.append(i)
-    print(max(L))
+    k.append(i)
+    print(max(k))
 
 
 sum = 67966 + 69727 + 69883
